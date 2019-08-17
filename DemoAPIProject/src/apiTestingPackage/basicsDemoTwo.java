e1c5a97c47a2116a4a722300d8f8dfdabf96591c
pranaymore926 <53988027+pranaymore926@users.noreply.github.com>
2019-08-17 11:36:48 +0530

    edited post body

--- DemoAPIProject/src/apiTestingPackage/basicsDemoTwo.java
+++ DemoAPIProject/src/apiTestingPackage/basicsDemoTwo.java
@@ -1,5 +1,7 @@
 package apiTestingPackage;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -11,15 +13,38 @@ public class basicsDemoTwo
 	@Test
 	public void postData()
 	{
-		RestAssured.baseURI = "http://216.10.245.166/" ;
+		RestAssured.baseURI = "http://216.10.245.166" ;
 		
 		given().
 		queryParam("key", "qaclick123").
 		body("{\r\n" + 
-				"    \\\"place_id\\\":\\\"5bd6898ac4da7b05e89e49f4f20e67d0\\\"           \r\n" + 
+				"\r\n" + 
+				"    \"location\":{\r\n" + 
+				"\r\n" + 
+				"        \"lat\" : -38.383494,\r\n" + 
+				"\r\n" + 
+				"        \"lng\" : 33.427362\r\n" + 
+				"\r\n" + 
+				"    },\r\n" + 
+				"\r\n" + 
+				"    \"accuracy\":50,\r\n" + 
+				"\r\n" + 
+				"    \"name\":\"Frontline house\",\r\n" + 
+				"\r\n" + 
+				"    \"phone_number\":\"(+91) 983 893 3937\",\r\n" + 
+				"\r\n" + 
+				"    \"address\" : \"29, side layout, cohen 09\",\r\n" + 
+				"\r\n" + 
+				"    \"types\": [\"shoe park\",\"shop\"],\r\n" + 
+				"\r\n" + 
+				"    \"website\" : \"http://google.com\",\r\n" + 
+				"\r\n" + 
+				"    \"language\" : \"French-IN\"\r\n" + 
+				"\r\n" + 
 				"}").
-		when().post("maps/api/place/add/json").
-		then().assertThat().statusCode(200).and().contentType(ContentType.JSON);
+		when().post("/maps/api/place/add/json").
+		then().assertThat().statusCode(200).and().
+		contentType(ContentType.JSON).and().body("status", equalTo("OK"));
 		
 
 }
