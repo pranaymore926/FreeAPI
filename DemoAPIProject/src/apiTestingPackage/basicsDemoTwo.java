d983f782f4102530d07e65bd28e8c117a8720118
pranaymore926 <53988027+pranaymore926@users.noreply.github.com>
2019-08-17 11:22:08 +0530

    modified post program

--- DemoAPIProject/src/apiTestingPackage/basicsDemoTwo.java
+++ DemoAPIProject/src/apiTestingPackage/basicsDemoTwo.java
@@ -3,19 +3,23 @@ package apiTestingPackage;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
 import static io.restassured.RestAssured.given;
 public class basicsDemoTwo 
 {
 	@Test
 	public void postData()
 	{
-		RestAssured.baseURI = "http://216.10.245.166/maps/api/place/add/json" ;
+		RestAssured.baseURI = "http://216.10.245.166/" ;
 		
 		given().
 		queryParam("key", "qaclick123").
 		body("{\r\n" + 
 				"    \\\"place_id\\\":\\\"5bd6898ac4da7b05e89e49f4f20e67d0\\\"           \r\n" + 
-				"}");
+				"}").
+		when().post("maps/api/place/add/json").
+		then().assertThat().statusCode(200).and().contentType(ContentType.JSON);
 		
 
 }
