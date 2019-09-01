451c77bdb1f69709eb20b67e12a51767a9788a81
pranaymore926 <53988027+pranaymore926@users.noreply.github.com>
2019-09-01 10:30:35 +0530

    Date:1stSept,2019

--- DemoAPIProject/src/apiTestingPackage/basicDemoThree.java
+++ DemoAPIProject/src/apiTestingPackage/basicDemoThree.java
@@ -2,8 +2,20 @@ package apiTestingPackage;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
+import files.Resources;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import files.PayLoad;
 
+
+import javax.annotation.Resource;
+
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -12,41 +24,27 @@ import io.restassured.response.Response;
 
 public class basicDemoThree 
 {
+	Properties prop = new Properties();
+	@BeforeTest
+	public void getData() throws IOException
+	{
+		
+		FileInputStream fis = new FileInputStream("C:\\Users\\Pranay\\git\\FreeAPI\\DemoAPIProject\\src\\files\\env.properties");
+		
+		prop.load(fis);
+		
+	}
 	@Test
 	public void addAndDelete()
 	{
-		String b = "{\r\n" + 
-				"\r\n" + 
-				"    \"location\":{\r\n" + 
-				"\r\n" + 
-				"        \"lat\" : -38.383494,\r\n" + 
-				"\r\n" + 
-				"        \"lng\" : 33.427362\r\n" + 
-				"\r\n" + 
-				"    },\r\n" + 
-				"\r\n" + 
-				"    \"accuracy\":50,\r\n" + 
-				"\r\n" + 
-				"    \"name\":\"Frontline house\",\r\n" + 
-				"\r\n" + 
-				"    \"phone_number\":\"(+91) 983 893 3937\",\r\n" + 
-				"\r\n" + 
-				"    \"address\" : \"29, side layout, cohen 09\",\r\n" + 
-				"\r\n" + 
-				"    \"types\": [\"shoe park\",\"shop\"],\r\n" + 
-				"\r\n" + 
-				"    \"website\" : \"http://google.com\",\r\n" + 
-				"\r\n" + 
-				"    \"language\" : \"French-IN\"\r\n" + 
-				"\r\n" + 
-				"}";
-RestAssured.baseURI = "http://216.10.245.166" ;
+		
+RestAssured.baseURI = prop.getProperty("HOST"); ;
 		
 //Grab the response 
 		Response res = given().
-		queryParam("key", "qaclick123").
-		body(b).
-		when().post("/maps/api/place/add/json").
+		queryParam("key", prop.getProperty("KEY")).
+		body(PayLoad.postData()).
+		when().post(Resources.placePostData()).
 		then().assertThat().statusCode(200).and().
 		contentType(ContentType.JSON).and().body("status", equalTo("OK"))
 		.extract().response();
